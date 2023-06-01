6d2f37809bc7ab447622bde1cd6c9c898330263e
Wendell Borges <62032363+Wendell00@users.noreply.github.com>
2023-05-31 21:42:12 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import {Navbar} from './components/navbar'
 import {AsideBar} from './components/asidebar'
 import {MainYT} from './components/main'
-import { Helmet } from 'react-helmet';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +15,6 @@ export default function Home() {
         <AsideBar/>
         <MainYT/>
       </main>
-      <Helmet>
-        <title>WenDEUS - YouTube</title>
-      </Helmet>
     </>
   )
 }
